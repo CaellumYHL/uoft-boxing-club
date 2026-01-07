ac1cd4f5c11f9005da09c74a68eaa57c2c7276eb
CaellumYHL <caecaeyhl@gmail.com>
2026-01-07 00:14:40 -0500

    Fix: Added actual frontend files

---
+++ frontend/next.config.ts
@@ -0,0 +1,12 @@
+import type { NextConfig } from "next";
+
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  output: 'export',      // Tells Next.js to build static HTML
+  distDir: 'out',        // Tells it to put files in the folder Firebase looks for
+  images: {
+    unoptimized: true,   // Essential for Firebase Hosting
+  },
+};
+
+export default nextConfig;
