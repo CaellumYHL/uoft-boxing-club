7f6d29c8e59d0a8a92f55243f096bb9de0714060
CaellumYHL <caecaeyhl@gmail.com>
2026-02-17 21:07:39 -0500

    fixing css file pushes

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -3,10 +3,11 @@ import type { NextConfig } from "next";
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'export',      // Tells Next.js to build static HTML
+  basePath: '/uoft-boxing-club',  // Required for GitHub Pages (repo name subpath)
   // distDir: 'out',        // Tells it to put files in the folder Firebase looks for
 
   images: {
-    unoptimized: true,   // Essential for Firebase Hosting
+    unoptimized: true,
   },
 };
 
