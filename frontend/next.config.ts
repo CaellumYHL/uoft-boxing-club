51dfa3bb5a1707dfa6e0feeda143aa3951012996
CaellumYHL <caecaeyhl@gmail.com>
2026-02-18 19:00:10 -0500

    fixed ico assets

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -6,6 +6,7 @@ const isProd = process.env.NODE_ENV === 'production';
 const nextConfig = {
   output: 'export',      // Tells Next.js to build static HTML
   basePath: isProd ? '/uoft-boxing-club' : '',  // Only use basePath for GitHub Pages
+  assetPrefix: isProd ? '/uoft-boxing-club/' : undefined,  // Ensure assets load from correct path
   // distDir: 'out',        // Tells it to put files in the folder Firebase looks for
 
   images: {
